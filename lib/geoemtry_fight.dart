62891285980a44ff7b77dab8ec6eb81822f580ad
fwoodmann <43883108+fwoodmann@users.noreply.github.com>
2024-08-02 18:12:09 +0200

    changed player component to postion component and added basic dragging of the player

--- lib/geoemtry_fight.dart
+++ lib/geoemtry_fight.dart
@@ -1,21 +1,33 @@
 import 'dart:async';
 
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:geometry_fight/player.dart';
 
-class GeoemtryFight extends FlameGame {
-  GeoemtryFight({super.children});
-
+class GeoemtryFight extends FlameGame with PanDetector {
+  late Player player;
   @override
   FutureOr<void> onLoad() {
-    super.onLoad();
-    world.add(Player(
-        position: Vector2(0, 0), height: 50.0, width: 50.0, color: Colors.red));
+    player = Player()
+      ..position = size / 2
+      ..width = 50
+      ..height = 100
+      ..anchor = Anchor.center;
+
+    add(player);
+    return super.onLoad();
   }
 
   @override
-  Color backgroundColor() {
-    return Colors.amber.shade100;
+  void onPanUpdate(DragUpdateInfo info) {
+    // TODO: implement onPanUpdate
+    player.move(info.delta.global);
   }
+}
+
+@override
+Color backgroundColor() {
+  return Colors.amber.shade100;
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,17 +1,15 @@
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
-class Player extends RectangleComponent {
-  Player(
-      {required super.position,
-      required double height,
-      required double width,
-      Color color = Colors.red})
-      : super(
-          anchor: Anchor.center,
-          size: Vector2(height, width),
-          paint: Paint()
-            ..color = color
-            ..style = PaintingStyle.fill,
-        );
+class Player extends PositionComponent {
+  static final _paint = Paint()..color = Colors.red;
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
+  }
+
+  void move(Vector2 delta) {
+    position.add(delta);
+  }
 }
